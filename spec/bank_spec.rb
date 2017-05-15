eee100049e41b3e1ab6e4be0bab2f21ee467826d
Eugenia Guerrero <eugenia.guerrero@me.com>
2017-05-15 11:45:11 +0100

    adds transactions array to store transaction history

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -4,19 +4,28 @@ describe Bank do
 
   subject(:bank){described_class.new}
 
-  it 'has an initial balance of zero' do
-    expect(bank.balance).to eq(0)
+  context 'when intialized' do
+    it 'has an initial balance of zero' do
+      expect(bank.balance).to eq(0)
+    end
+
+    it 'has a list of transactions' do
+      expect(bank.transactions).to eq []
+    end
   end
 
-  it 'can make a deposit' do
-    bank.deposit(1000)
-    expect(bank.balance).to eq(1000)
+  context 'making transactions' do
+    it 'can make a deposit' do
+      bank.deposit(1000)
+      expect(bank.balance).to eq(1000)
+    end
+
+    it 'can make a withdrawal' do
+      bank.deposit(1000)
+      bank.withdrawal(500)
+      expect(bank.balance).to eq(500)
+    end
   end
 
-  it 'can make a withdrawal' do
-    bank.deposit(1000)
-    bank.withdrawal(500)
-    expect(bank.balance).to eq(500)
-  end
 
 end
