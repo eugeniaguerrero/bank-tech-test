3ea97ea649094036f58f9b0b1e698c9b0c4baa03
Eugenia Guerrero <eugenia.guerrero@me.com>
2017-05-15 15:26:21 +0100

    adds capacity to see recent transactions

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,22 +1,29 @@
+require 'date'
+
 class Bank
 
   attr_reader :balance, :transactions
 
-  def initialize
-    @balance = 0
+  INITIAL_BALANCE = 0
+
+  def initialize(balance = INITIAL_BALANCE)
     @transactions = []
+    @balance = balance
   end
 
   def deposit(amount)
     @balance += amount
+    @transactions.push(date: Date.today, amount: amount, balance: balance)
   end
 
   def withdrawal(amount)
     @balance -= amount
+    @transactions.push(date: Date.today, amount: -amount, balance: balance)
   end
+
 
   private
 
-  attr_writer :balance
+  attr_writer :balance, :transactions
 
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -19,11 +19,31 @@ describe Bank do
       bank.deposit(1000)
       expect(bank.balance).to eq(1000)
     end
-
     it 'can make a withdrawal' do
       bank.deposit(1000)
       bank.withdrawal(500)
       expect(bank.balance).to eq(500)
+    end
+  end
+
+  context 'seeing transactions' do
+    it 'shows the time of the deposit' do
+      bank.deposit(1000)
+      expect(bank.transactions).to include
+      [{:date=>Date.today,
+      :amount=>1000,
+      :balance=>1000}]
+    end
+    it 'shows a list of transactions' do
+      bank.deposit(1000)
+      bank.withdrawal(500)
+      expect(bank.transactions).to include
+      [{:date=>Date.today,
+      :amount=>1000,
+      :balance=>1000},
+      {:date=>Date.today,
+      :amount=>-500,
+      :balance=>500}]
     end
   end
 
